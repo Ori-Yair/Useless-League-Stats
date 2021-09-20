d65290205dfd6cbbe71bcba3b47958592b3a6779
Ori Yair <oriyair14@gmail.com>
2021-09-20 15:51:58 +0300

    Basic riotwatcher connection

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+import pandas
+
+from riotwatcher import LolWatcher, ApiError
+
 
 def main():
-    print("Hello, world!")
+    watcher = LolWatcher(get_key())
+    region = ''
+    summoner = ''
+
+    summoner_info = watcher.summoner.by_name(region, summoner)
+    print(summoner_info)
+
+
+def get_key():
+    with open('api-key', 'r') as key_file:
+        return key_file.read()
 
 
 if __name__ == '__main__':
